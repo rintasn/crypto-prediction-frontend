6e9c7444ff8344c5ecdb271a8c977aa81cb1a69d
Rinta Setyo Nugroho <rinta.setyonugroho@gmail.com>
2025-02-24 11:17:17 +0700

    update

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -185,7 +185,7 @@ const CryptoPrediction: React.FC = () => {
     setError(null);
 
     try {
-      const response = await fetch('http://localhost:8000/predict', {
+      const response = await fetch('https://portal4.incoe.astra.co.id/api-analysis/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
